07dbc4b1183f62f0175f67ca05f4908a7994368e
jessica.l.hamilton <jessica.l.hamilton@bda7e703-2156-0410-93e3-99e0a704cdff>
2009-04-30 06:10:46 +0000

    Thought I'd try make it weirdly functional and use Monad-style combinators.

--- kernel/e1000.ml
+++ kernel/e1000.ml
@@ -32,8 +32,6 @@ end
 
 let node_address_size = 6
 
-(* assume regAddr is resources.(0) *)
-
 module E1000 = struct
     let status = 0x08
     module EECD = struct
@@ -49,163 +47,117 @@ module E1000 = struct
     let read_opcode = 0x6
 end
 
-(*raise_ee_clock
-lower_ee_clock
-shift_out_ee_bits
-shift_in_ee_bits
-acquire_eeprom
-release_eeprom
-standby_eeprom
-read_eeprom*)
-
-(*#define E1000_READ_OFFSET(hw, offset) \
-        (*(volatile uint32 *)((char * )(((struct em_osdep * )(hw)->back)->dev->regAddr) + offset))
-#define E1000_WRITE_OFFSET(hw, offset, value) \
-        (*(volatile uint32 * )((char * )(((struct em_osdep * )(hw)->back)->dev->regAddr) + offset) = value)
-#define E1000_REG_OFFSET(hw, reg) \
-    ((hw)->mac_type >= em_82543 ? E1000_##reg : E1000_82542_##reg)
-    
-E1000_READ_REG
-#define E1000_READ_REG(hw, reg) \
-    E1000_READ_OFFSET(hw, E1000_REG_OFFSET(hw, reg))
-E1000_WRITE_REG
-#define E1000_WRITE_REG(hw, reg, value) \
-    E1000_WRITE_OFFSET(hw, E1000_REG_OFFSET(hw, reg), value)
-E1000_WRITE_FLUSH
-#define E1000_WRITE_FLUSH(a) \
-        E1000_READ_REG(a, STATUS) *)*)
-        
 let usec_delay () =
     for i = 1 to EEPROM.delay_usec do
         Asm.out8 0x80 0x80
     done
 
-let read_reg = Asm.peek32_offset
-let write_reg = Asm.poke32_offset
-let write_flush eeprom = ignore (Asm.peek32_offset eeprom E1000.status)
-
-let raise_ee_clk eeprom eecd =
-    let eecd = eecd |! E1000.EECD.sk in
-    write_reg eeprom E1000.eecd eecd;
-    write_flush eeprom;
-    usec_delay ();
-    eecd
-
-let lower_ee_clk eeprom eecd =
-    let eecd = eecd &! ~!E1000.EECD.sk in
-    write_reg eeprom E1000.eecd eecd;
-    write_flush eeprom;
-    usec_delay ();
-    eecd
-
-let rec accum n f r = if n = 0 then r else accum (n-1) f (f r)
-
-let shift_out_ee_bits eeprom data count =
-    let (eecd, _) = accum count begin fun (eecd,mask) ->
-        let eecd = eecd &! ~!E1000.EECD.di in
-        let eecd =
-            if data land mask <> 0 then eecd |! E1000.EECD.di else eecd in
-        write_reg eeprom E1000.eecd eecd;
-        write_flush eeprom;
-        usec_delay ();
-        ((lower_ee_clk eeprom (raise_ee_clk eeprom eecd)), mask lsr 1)
-    end (((read_reg eeprom E1000.eecd) &! ~!E1000.EECD._do), 1 lsl (count - 1)) in
-    (* we leave the DI bit set to 0 when we leave this routine *)
-    write_reg eeprom E1000.eecd (eecd &! ~!E1000.EECD.di)
-
-let shift_in_ee_bits eeprom count =
-    let _, data = accum count begin fun (eecd, data) ->
-        let data = data lsl 1 in
-        ignore (raise_ee_clk eeprom eecd);
-        let eecd = read_reg eeprom E1000.eecd in
-        let eecd = eecd &! ~!E1000.EECD.di in
-        let data =
-            if eecd &! E1000.EECD._do <> zero then data lor 1 else data in
-        lower_ee_clk eeprom eecd, data
-    end (((read_reg eeprom E1000.eecd) &! ~!(E1000.EECD._do |! E1000.EECD.di)), 0) in
-    data
-
-let standby_eeprom eeprom =
-    let eecd = read_reg eeprom E1000.eecd in
-    let x = [
-        (fun eecd -> eecd &! ~!(E1000.EECD.cs |! E1000.EECD.sk));
-        (fun eecd -> eecd |! E1000.EECD.sk);  (* clock high *)
-        (fun eecd -> eecd |! E1000.EECD.cs);  (* select eeprom *)
-        (fun eecd -> eecd &! ~!E1000.EECD.sk) (* clock low *)
-    ] in
-    ignore (List.fold_left (fun eecd f ->
-        let eecd = f eecd in
-        write_reg eeprom E1000.eecd eecd;
-        write_flush eeprom;
-        usec_delay ();
-        eecd) eecd x)
-        
-let acquire_eeprom eeprom =
-    let eecd = read_reg eeprom E1000.eecd in
-    (* request eeprom access *)
-    let eecd = eecd |! E1000.EECD.req in
-    write_reg eeprom E1000.eecd eecd;
-    let limit = ref 0 in
-    while ((read_reg eeprom E1000.eecd) &! E1000.EECD.gnt = zero) && !limit < E1000.grant_attempts do
-        incr limit;
-        (* delay 5usec *)
-        usec_delay ();
-    done;
-    if (read_reg eeprom E1000.eecd) &! E1000.EECD.gnt = zero then begin
-        write_reg eeprom E1000.eecd (eecd &! ~!E1000.EECD.req);
-        failwith "Could not acquire EEPROM grant";
-    end;
-    (* setup eeprom for read/write *)
-    (* clear sk and di *)
-    let eecd = eecd &! ~!(E1000.EECD.di |! E1000.EECD.sk) in
-    write_reg eeprom E1000.eecd eecd;
-    (* set cs *)
-    let eecd = eecd |! E1000.EECD.cs in
-    write_reg eeprom E1000.eecd eecd
-    (* done! *)
-
-(* read eeprom *)
-
-let read_eeprom eeprom offset words =
-    if offset > EEPROM.word_size || words > EEPROM.word_size - offset || words = 0 then
-        failwith "Invalid parameters supplied";
-    acquire_eeprom eeprom;
-    let data = Array.make words 0 in
-    (* assuming microwire eeprom *)
-    for i = 0 to words - 1 do
-        shift_out_ee_bits eeprom E1000.read_opcode EEPROM.opcode_bits;
-        shift_out_ee_bits eeprom (offset + i) EEPROM.address_bits;
-        data.(i) <- shift_in_ee_bits eeprom 16;
-        standby_eeprom eeprom;
-    done;
-    (*release_eeprom eeprom;*)
-    data
-
-(* read mac address *)
-
-let read_mac_addr eeprom =
-    let mac_addr = [| 0; 0; 0; 0; 0; 0 |] in
-    for i = 0 to 2 do (* 0 to node_address_size / 2 - 1 *)
-        let data = read_eeprom eeprom ((i * 2) lsr 1) 1 in
-        mac_addr.(i * 2) <- data.(0) land 0x00FF;
-        mac_addr.(i * 2 + 1) <- data.(0) lsr 8;
-    done;
-    mac_addr
-
-(* create the driver instance *)
-
 let get_resource device = match device.resources.(0) with
     | Memory mem -> mem
     | _ -> failwith "Invalid resource type"
 
+let (>>=) x f = f x
+let wrap f x = f x; x
+let lift f x = f (); x
+
+let rec accum n f r = if n = 0 then r else accum (n-1) f (f r)
+let rec accum_n n f r = if n = 0 then r else accum_n (n-1) f (f n r)
+
+(* create the driver instance *)
+
 let create device =
-    let resource = get_resource device in
-    Asm.poke32 resource ((Asm.peek32 resource) |! 0x40l);
-    let mac_addr = read_mac_addr resource in
-    Vt100.printf "mac_addr: %02x:%02x:%02x:%02x:%02x:%02x\n"
+    (* get the location of the EEPROM *)
+    let eeprom = get_resource device in
+    (* and some helper functions to read/write to the EEPROM *)
+    let read () = Asm.peek32_offset eeprom E1000.eecd in
+    let write value = Asm.poke32_offset eeprom E1000.eecd value in
+    let flush () = ignore (Asm.peek32_offset eeprom E1000.status) in
+    (* nic stuff, done here to use the helper methods *)
+    let module E = struct
+        let raise_clock eecd =
+            eecd |! E1000.EECD.sk >>=
+            wrap write >>=
+            lift flush >>=
+            lift usec_delay
+        
+        let lower_clock eecd =
+            eecd &! ~!E1000.EECD.sk >>=
+            wrap write >>=
+            lift flush >>=
+            lift usec_delay
+        
+        let standby () =
+            let k1 = fun x -> x &! ~!(E1000.EECD.cs |! E1000.EECD.sk)
+            and k2 = fun x -> x |! E1000.EECD.sk
+            and k3 = fun x -> x |! E1000.EECD.cs
+            and k4 = fun x -> x &! ~!E1000.EECD.sk in
+            read () >>=
+            k1 >>= wrap write >>= lift flush >>= lift usec_delay >>=
+            k2 >>= wrap write >>= lift flush >>= lift usec_delay >>=
+            k3 >>= wrap write >>= lift flush >>= lift usec_delay >>=
+            k4 >>= wrap write >>= lift flush >>= lift usec_delay >>=
+            ignore
+        
+        let output data count =
+            let bits = Array.init count begin fun n -> data land (1 lsl n) <> 0 end in
+            (read ()) &! ~!E1000.EECD._do >>=
+            accum_n count begin fun n x ->
+                (* need to start at right-most array entry, so bits.(n-1) will work *)
+                (if bits.(n-1) then x |! E1000.EECD.di else x &! ~!E1000.EECD.di) >>=
+                wrap write >>= lift flush >>= lift usec_delay >>=
+                raise_clock >>= lower_clock
+            end >>=
+            (fun x -> x &! ~!E1000.EECD.di) >>=
+            write
+        
+        let input count =
+            let data = ref 0 in
+            (read ()) &! ~!(E1000.EECD._do |! E1000.EECD.di) >>=
+            accum count begin fun x ->
+                data := !data lsl 1;
+                ignore (raise_clock x);
+                let x = (read ()) &! ~!E1000.EECD.di in
+                if x &! E1000.EECD._do <> zero then data := !data lor 1;
+                lower_clock x
+            end >>= ignore;
+            !data
+        
+        let acquire () =
+            read () |! E1000.EECD.req >>= wrap write >>= begin fun x ->
+                let limit = ref 0 in
+                while read () &! E1000.EECD.gnt = zero && !limit < E1000.grant_attempts do
+                    incr limit; usec_delay ();
+                done;
+                if !limit = E1000.grant_attempts then begin
+                    write (x &! ~!E1000.EECD.req);
+                    failwith "e1000: unable to acquire EEPROM";
+                end;
+                x &! ~!(E1000.EECD.di |! E1000.EECD.sk)
+            end >>=
+            wrap write >>=
+            begin fun x -> x |! E1000.EECD.cs end >>=
+            write
+        
+        let read_word offset = (* special case when reading single word *)
+            (* sanity checking omitted *)
+            acquire ();
+            output E1000.read_opcode EEPROM.opcode_bits;
+            output offset EEPROM.address_bits;
+            input 16 >>= lift standby
+            (* we still need to release the EEPROM :P *)
+        end
+    in
+    (* get the mac address *)
+    let mac_addr = [| 0; 0; 0; 0; 0; 0 |] in
+    for i = 0 to 2 do
+        let data = E.read_word ((i * 2) lsr 1) in
+        mac_addr.(i*2)   <- data land 0xFF;
+        mac_addr.(i*2+1) <- data lsr 8;
+    done;
+    (* print the mac address *)
+    Vt100.printf "e1000: mac addr: %02x:%02x:%02x:%02x:%02x:%02x\n"
         mac_addr.(0) mac_addr.(1) mac_addr.(2)
-        mac_addr.(3) mac_addr.(4) mac_addr.(5);
-    ()
+        mac_addr.(3) mac_addr.(4) mac_addr.(5)
 
 (* init stuff *)
 
