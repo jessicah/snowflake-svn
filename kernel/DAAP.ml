f3d3e490dc382b36c2138cdd674baf67bf90c50a
jessica.l.hamilton@gmail.com <jessica.l.hamilton@gmail.com@bda7e703-2156-0410-93e3-99e0a704cdff>
2009-05-11 11:12:13 +0000

    This is some crazy alteration of the parsing for DAAP... It's still parsing the server-info correctly, so that's a plus!

--- kernel/DAAP.ml
+++ kernel/DAAP.ml
@@ -7,47 +7,165 @@ let rec replace_all s f r = match S.replace s f r with
 
 (* daap format: 4-byte tag : 4-byte length : length bytes data *)
 
-let parse_tag bits = bitmatch bits with {
+type kind = Byte | Short | Int | Long | String | Date | Version | List
+
+(* lookup table of types *)
+let types = [
+	"mstt", Int;
+	"miid", Int;
+	"minm", String;
+	"mikd", Byte;
+	"mper", Long;
+	"mcon", List;
+	"mcti", Int;
+	"mpco", Int;
+	"msts", String;
+	"mimc", Int;
+	"mrco", Int;
+	"mtco", Int;
+	"mlcl", List;
+	"mlit", List;
+	"mbcl", List;
+	"mdcl", List;
+	"msrv", List;
+	(*msaud*)
+	"mslr", Byte;
+	"mpro", Version;
+	"apro", Version;
+	"msal", Byte;
+	"msup", Byte;
+	"mspi", Byte;
+	"msex", Byte;
+	"msbr", Byte;
+	"msqy", Byte;
+	"msix", Byte;
+	"msrs", Byte;
+	"mstm", Int;
+	"msdc", Int;
+	"mccr", List;
+	"mcnm", Int;
+	"mcna", String;
+	"mcty", Short;
+	"mlog", List;
+	"mlid", Int;
+	"mupd", List;
+	"msur", Int;
+	"muty", Byte;
+	"mudl", List;
+	"avdb", List;
+	"abro", List;
+	"abal", List;
+	"abar", List;
+	"abcp", List;
+	"abgn", List;
+	"adbs", List;
+	"asal", String;
+	"asar", String;
+	"asbt", Short;
+	"asbr", Short;
+	"ascm", String;
+	"asco", Byte;
+	"asda", Date;
+	"asdm", Date;
+	"asdc", Short;
+	"asdn", Short;
+	"asdb", Byte;
+	"aseq", String;
+	"asfm", String;
+	"asgn", String;
+	"asdt", String;
+	"asrv", Byte;
+	"assr", Int;
+	"assz", Int;
+	"asst", Int;
+	"assp", Int;
+	"astm", Int;
+	"astc", Short;
+	"astn", Short;
+	"asur", Byte;
+	"asyr", Short;
+	"asdk", Byte;
+	"asul", String;
+	"aply", List;
+	"apbl", Byte;
+	"apso", List;
+	"prsv", List;
+	"arif", List;
+	"aeNV", Int;
+	"aeSP", Byte;
+]
+
+type result = B of int | Sh of int | I of int32 | L of int64 | S of string | D of Bitstring.t | V of int * int | Ls of (string * result) list | U of Bitstring.t
+
+let parse bits = bitmatch bits with {
 		tag : 32 : string;
 		length : 32;
 		data : (Int32.to_int length) * 8 : bitstring;
 		rest : -1 : bitstring
 	} -> tag, data, rest
 
-let rec parse_list bits acc =
-	let (tag,data,rest) = parse_tag bits in
-	if Bitstring.bitstring_length rest = 0 then
-		(tag,data) :: acc
+let rec parse_kind tag data =
+	if List.mem_assoc tag types then
+		match List.assoc tag types with
+		| Byte -> parse_byte tag data
+		| Short -> parse_short tag data
+		| Int -> parse_int tag data
+		| Long -> parse_long tag data
+		| String -> parse_string tag data
+		| List -> parse_list tag data
+		| Date -> parse_date tag data
+		| Version -> parse_version tag data
 	else
-		parse_list rest ((tag,data)::acc)
-
-let parse_byte bits = bitmatch bits with {
+		tag, U data
+and parse_byte tag bits = bitmatch bits with {
 		b : 8 : int
-	} -> b
+	} -> tag, B b
+and parse_short tag bits = bitmatch bits with {
+		sh : 16 : int
+	} -> tag, Sh sh
+and parse_int tag bits = bitmatch bits with {
+		i : 32
+	} -> tag, I i
+and parse_long tag bits = bitmatch bits with {
+		l : 64
+	} -> tag, L l
+and parse_string tag bits =
+	tag, S (Bitstring.string_of_bitstring bits)
+and parse_list tag bits =
+	tag, Ls (parse_list_acc bits [])
+and parse_list_acc bits acc =
+	if Bitstring.bitstring_length bits = 0 then acc
+	else begin
+		let tag, data, rest = parse bits in
+		parse_list_acc rest ((parse_kind tag data) :: acc)
+	end
+and parse_date tag bits =
+	tag, D bits
+and parse_version tag bits = bitmatch bits with {
+		minor : 16 : int; major : 16 : int
+	} -> tag, V (major, minor)
 
-let parse_int bits = bitmatch bits with {
-		num : 32
-	} -> num
+let rec kind_to_string = function
+	| B i | Sh i -> Printf.sprintf "%d" i
+	| I i -> Printf.sprintf "%ld" i
+	| L i -> Printf.sprintf "%Ld" i
+	| S s -> replace_all s "\xE2\x80\x99" "'"
+	| D b -> Bitstring.string_of_bitstring b
+	| V (maj,min) -> Printf.sprintf "%d.%d" maj min
+	| Ls l ->
+		let ll = List.map kind_to_string (List.map snd l) in
+		Printf.sprintf "[%s]" (String.concat ", " ll)
+	| U b -> Bitstring.string_of_bitstring b
 
-let parse_version bits = bitmatch bits with {
-		a : 8; b : 8; c : 8; d : 8
-	} -> d, c, b, a
-
-let print_tag_data tag data = match tag with
-	| "msdc" -> Printf.sprintf "%ld" (parse_int data)
-	| "minm" -> replace_all (Bitstring.string_of_bitstring data) "\xE2\x80\x99" "'"
-	| "mpro" | "apro" -> let a,b,c,d = parse_version data in
-		Printf.sprintf "%d.%d.%d.%d" a b c d
-	| _ -> "<unknown>"
-
-let print_tag (tag,data) map = match List.mem_assoc tag map with
-	| true ->
-		Vt100.printf "  %s: %s\n"
-			(List.assoc tag map)
-			(print_tag_data tag data)
+let rec print (tag,kind) fields =
+	if List.mem_assoc tag fields then begin
+		Vt100.printf "  %s: %s\n" (List.assoc tag fields) (kind_to_string kind)
+	end else begin match kind with
+	| Ls l -> List.iter (fun x -> print x fields) l
 	| _ -> ()
+	end
 
-let features = [
+(*let features = [
 		"msix", "index"; "msex", "extensions"; "msup", "update";
 		"msal", "auto-logout"; "mslr", "requires-logon";
 		"msqy", "query"; "msrs", "resolve"; "msbr", "browse";
@@ -60,20 +178,19 @@ let rec supported_features acc = function
 	if List.mem_assoc tag features && parse_byte data <> 0 then
 		supported_features (List.assoc tag features::acc) xs
 	else
-		supported_features acc xs
+		supported_features acc xs*)
 
 let parse_server_info server_info =
-	let (tag,data,rest) = parse_tag (Bitstring.bitstring_of_string server_info) in
+	let tag, data, rest = parse (Bitstring.bitstring_of_string server_info) in
 	if String.compare tag "msrv" <> 0 then
 		failwith "daap: expected server info response";
 	if Bitstring.bitstring_length rest <> 0 then
 		Vt100.printf "daap: warning, trailing data found\n";
-	let params = List.rev (parse_list data []) in
-	let features = supported_features [] params in
+	let daap = parse_kind tag data in
 	Vt100.printf "Server Info:\n";
-	List.iter begin fun tuple ->
-		print_tag tuple ["mpro", "M-Version"; "apro", "A-Version"; "minm", "Library Name"; "msdc", "Database Count"]
-	end params;
-	Vt100.printf "  Supported Features:";
-	List.iter (Vt100.printf " %s") features;
-	Vt100.printf "\n";
+	print daap [
+		"mpro", "M-Version";
+		"apro", "A-Version";
+		"minm", "Library Name";
+		"msdc", "Database Count";
+	]
