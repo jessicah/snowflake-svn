5f7c77128dba032b7341cae4c64721e1264c6c50
jessica.l.hamilton <jessica.l.hamilton@bda7e703-2156-0410-93e3-99e0a704cdff>
2009-08-31 07:00:43 +0000

    Used some regex and commandline tools to create my record type fairly automagically :) Still works! And my record type definition hack ftw!

--- kernel/ext2fs.ml
+++ kernel/ext2fs.ml
@@ -5,6 +5,64 @@
 open Partitions
 open IO
 
+(* note: order of fields is reversed compared to order on disk *)
+type superblock = {
+	(* other options *)
+	reserved_3 : int list;
+	s_first_meta_bg : int32;
+	s_default_mount_options : int32;
+	(* directory indexing support *)
+	reserved_2 : int list;
+	s_def_hash_version : int;
+	hash_seed : int list;
+	(* journalling support *)
+	s_last_orphan : int32;
+	s_journal_dev : int32;
+	s_journal_inum : int32;
+	s_journal_uuid : int list;
+	(* performance hints *)
+	reserved_1 : int list;
+	s_prealloc_dir_blocks : int;
+	s_prealloc_blocks : int;
+	(* ext2_dynamic_rev specific... *)
+	s_algo_bitmap : int32;
+	s_last_mounted : int list;
+	s_volume_name : int list;
+	s_uuid : int list;
+	s_feature_ro_compat : int32;
+	s_feature_incompat : int32;
+	s_feature_compat : int32;
+	s_block_group_nr : int;
+	s_inode_size : int;
+	s_first_ino : int;
+	(* typical stuff :) *)
+	s_def_resgid : int;
+	s_def_resuid : int;
+	s_rev_level : int32;
+	s_creator_os : int32;
+	s_checkinterval : int;
+	s_lastcheck : int32;
+	s_minor_rev_level : int;
+	s_errors : int;
+	s_state : int;
+	s_magic : int;
+	s_max_mnt_count : int;
+	s_mnt_count : int;
+	s_wtime : int32;
+	s_mtime : int32;
+	s_inodes_per_group : int;
+	s_frags_per_group : int;
+	s_blocks_per_group : int;
+	s_log_frag_size : int;
+	s_log_block_size : int;
+	s_first_data_block : int;
+	s_free_inodes_count : int;
+	s_free_blocks_count : int;
+	s_r_blocks_count : int;
+	s_blocks_count : int;
+	s_inodes_count : int;
+}
+
 let read_bytes i n =
 	let rec read acc = function
 		| 0 -> List.rev acc
@@ -18,74 +76,77 @@ let init p =
 	if p.info.code <> 0x83 then failwith "not linux native partition";
 	let superblock_raw = p.read 2 2 in
 	let i = IO.input_string superblock_raw in
-	let s_inodes_count = read_i32 i in
-	let s_blocks_count = read_i32 i in
-	let s_r_blocks_count = read_i32 i in
-	let s_free_blocks_count = read_i32 i in
-	let s_free_inodes_count = read_i32 i in
-	let s_first_data_block = read_i32 i in
-	let s_log_block_size = read_i32 i in
-	let s_log_frag_size = read_i32 i in
-	let s_blocks_per_group = read_i32 i in
-	let s_frags_per_group = read_i32 i in
-	let s_inodes_per_group = read_i32 i in
-	let s_mtime = read_real_i32 i in
-	let s_wtime = read_real_i32 i in
-	let s_mnt_count = read_i16 i in
-	let s_max_mnt_count = read_i16 i in
-	let s_magic = read_i16 i in
-	let s_state = read_i16 i in
-	let s_errors = read_i16 i in
-	let s_minor_rev_level = read_i16 i in
-	let s_lastcheck = read_real_i32 i in
-	let s_checkinterval = read_i32 i in
-	let s_creator_os = read_real_i32 i in
-	let s_rev_level = read_real_i32 i in
-	let s_def_resuid = read_i16 i in
-	let s_def_resgid = read_i16 i in
-	(* ext2_dynamic_rev specific... *)
-	let s_first_ino = read_i32 i in
-	let s_inode_size = read_i16 i in
-	let s_block_group_nr = read_i16 i in
-	let s_feature_compat = read_real_i32 i in
-	let s_feature_incompat = read_real_i32 i in
-	let s_feature_ro_compat = read_real_i32 i in
-	let s_uuid = read_bytes i 16 in
-	let s_volume_name = read_bytes i 16 in
-	let s_last_mounted = read_bytes i 64 in
-	let s_algo_bitmap = read_real_i32 i in
-	(* performance hints *)
-	let s_prealloc_blocks = read_byte i in
-	let s_prealloc_dir_blocks = read_byte i in
-	ignore (read_bytes i 2);
-	(* journalling support *)
-	let s_journal_uuid = read_bytes i 16 in
-	let s_journal_inum = read_real_i32 i in
-	let s_journal_dev = read_real_i32 i in
-	let s_last_orphan = read_real_i32 i in
-	(* directory indexing support *)
-	let hash_seed = read_bytes i 16 in
-	let s_def_hash_version = read_byte i in
-	ignore (read_bytes i 3);
-	(* other options *)
-	let s_default_mount_options = read_real_i32 i in
-	let s_first_meta_bg = read_real_i32 i in
-	(* rest. 760 bytes, is unused *)
+	let s = {
+		s_inodes_count = read_i32 i;
+		s_blocks_count = read_i32 i;
+		s_r_blocks_count = read_i32 i;
+		s_free_blocks_count = read_i32 i;
+		s_free_inodes_count = read_i32 i;
+		s_first_data_block = read_i32 i;
+		s_log_block_size = read_i32 i;
+		s_log_frag_size = read_i32 i;
+		s_blocks_per_group = read_i32 i;
+		s_frags_per_group = read_i32 i;
+		s_inodes_per_group = read_i32 i;
+		s_mtime = read_real_i32 i;
+		s_wtime = read_real_i32 i;
+		s_mnt_count = read_i16 i;
+		s_max_mnt_count = read_i16 i;
+		s_magic = read_i16 i;
+		s_state = read_i16 i;
+		s_errors = read_i16 i;
+		s_minor_rev_level = read_i16 i;
+		s_lastcheck = read_real_i32 i;
+		s_checkinterval = read_i32 i;
+		s_creator_os = read_real_i32 i;
+		s_rev_level = read_real_i32 i;
+		s_def_resuid = read_i16 i;
+		s_def_resgid = read_i16 i;
+		(* ext2_dynamic_rev specific... *)
+		s_first_ino = read_i32 i;
+		s_inode_size = read_i16 i;
+		s_block_group_nr = read_i16 i;
+		s_feature_compat = read_real_i32 i;
+		s_feature_incompat = read_real_i32 i;
+		s_feature_ro_compat = read_real_i32 i;
+		s_uuid = read_bytes i 16;
+		s_volume_name = read_bytes i 16;
+		s_last_mounted = read_bytes i 64;
+		s_algo_bitmap = read_real_i32 i;
+		(* performance hints *)
+		s_prealloc_blocks = read_byte i;
+		s_prealloc_dir_blocks = read_byte i;
+		reserved_1 = read_bytes i 2;
+		(* journalling support *)
+		s_journal_uuid = read_bytes i 16;
+		s_journal_inum = read_real_i32 i;
+		s_journal_dev = read_real_i32 i;
+		s_last_orphan = read_real_i32 i;
+		(* directory indexing support *)
+		hash_seed = read_bytes i 16;
+		s_def_hash_version = read_byte i;
+		reserved_2 = read_bytes i 3;
+		(* other options *)
+		s_default_mount_options = read_real_i32 i;
+		s_first_meta_bg = read_real_i32 i;
+		reserved_3 = read_bytes i 760;
+	} in
 	(* print something kinda like mke2fs *)
-	Vt100.printf "Block size=%d (log=%d)\n" (1024 lsl s_log_block_size) s_log_block_size;
-	Vt100.printf "Fragment size=%d (log=%d)\n" (1024 lsl s_log_frag_size) s_log_frag_size;
-	Vt100.printf "%d inodes, %d blocks\n" s_inodes_count s_blocks_count;
-	Vt100.printf "%d blocks (%d%%) reserved for the super user\n" s_r_blocks_count ((s_r_blocks_count+1) * 100 / s_blocks_count);
-	Vt100.printf "First data block=%d\n" s_first_data_block;
+	Vt100.printf "Block size=%d (log=%d)\n" (1024 lsl s.s_log_block_size) s.s_log_block_size;
+	Vt100.printf "Fragment size=%d (log=%d)\n" (1024 lsl s.s_log_frag_size) s.s_log_frag_size;
+	Vt100.printf "%d inodes, %d blocks\n" s.s_inodes_count s.s_blocks_count;
+	Vt100.printf "%d blocks (%d%%) reserved for the super user\n" s.s_r_blocks_count ((s.s_r_blocks_count+1) * 100 / s.s_blocks_count);
+	Vt100.printf "First data block=%d\n" s.s_first_data_block;
 	Vt100.printf "Maximum filesystem blocks=? dunno how to calculate...\n"; (* dunno where this comes from *)
-	Vt100.printf "%d block groups\n" (s_inodes_count / s_inodes_per_group); (* this is off by one :( *)
-	Vt100.printf "%d blocks per group, %d fragments per group\n" s_blocks_per_group s_frags_per_group;
-	Vt100.printf "%d inodes per group\n" s_inodes_per_group;
+	Vt100.printf "%d block groups\n" (s.s_inodes_count / s.s_inodes_per_group); (* this is off by one :( *)
+	Vt100.printf "%d blocks per group, %d fragments per group\n" s.s_blocks_per_group s.s_frags_per_group;
+	Vt100.printf "%d inodes per group\n" s.s_inodes_per_group;
 	Vt100.printf "Superblock backups stored on blocks: ....\n";
 	Vt100.printf "\n";
 	Vt100.printf "This filesystem will be automatically checked every %d mounts or %d days,\n"
-		s_max_mnt_count (s_checkinterval / 86400);
+		s.s_max_mnt_count (s.s_checkinterval / 86400);
 	Vt100.printf "whichever comes first.\n";
+	(* s is our superblock! *)
 		with Failure "not linux native partition" -> ()
 		| ex -> Vt100.printf "error: %s" (Printexc.to_string ex)
 	end
