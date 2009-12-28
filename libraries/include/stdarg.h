e91ff7336bcb67410f8bce5c1288233bc252d070
jessica.l.hamilton@gmail.com <jessica.l.hamilton@gmail.com@bda7e703-2156-0410-93e3-99e0a704cdff>
2009-12-28 22:44:59 +0000

    Modified stdarg.h to account for renaming of builtin gcc function with version 4.4+

--- libraries/include/stdarg.h
+++ libraries/include/stdarg.h
@@ -4,7 +4,11 @@
 #if (__GNUC__ > 2) || ((__GNUC__ == 2) && (__GNUC_MINOR__ >= 96))
 
 typedef __builtin_va_list va_list;
+#if (__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 4))
+#define va_start(v,l)	__builtin_va_start((v),l)
+#else
 #define va_start(v,l)	__builtin_stdarg_start((v),l)
+#endif
 #define va_end		__builtin_va_end
 #define va_arg		__builtin_va_arg
 #define __va_copy(d,s)	__builtin_va_copy((d),(s))
